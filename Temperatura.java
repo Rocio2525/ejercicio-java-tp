ce07c6646df9d7980c6df4c0c73d62c04cc35e6b
Rocio2525 <71531844+Rocio2525@users.noreply.github.com>
2020-10-07 15:39:27 -0300

    Add files via upload

---
+++ Temperatura.java
@@ -0,0 +1,125 @@
+
+ 
+package temperatura;
+
+public class Temperatura {
+
+    
+    public static void main(String[] args) {
+       /* 1) int temperatura=27;
+       boolean tem=true;
+       boolean llueve = true;
+
+if (tem) {
+
+if(temperatura > 25) {
+System.out.println("voy a la playa");
+       
+    }
+    
+}*/
+       /*2)
+       boolean hace_sol=true;
+       
+       if (hace_sol){
+         System.out.println("hace sol?");
+         System.out.println("No te olvides la sombrilla");
+         
+         
+
+         
+
+}*/
+      /* 3)boolean hace_sol =true;
+      boolean nevado = true;
+      if (hace_sol&&nevado);
+      System.out.println("nevado , hace sol");
+      System.out.println("que bien");*/
+      
+      /*4) boolean nevado=true;
+       int temperatura= 29;
+       
+       if(nevado&&temperatura>=20){
+       }
+       if(temperatura <=30){
+           System.out.println("No me lo creo");
+       }*/
+      /*5)boolean hace_sol=true;
+      int temperatura = 32;
+      
+      if(hace_sol){
+          if(temperatura <0){
+              
+          }
+          if (temperatura >30 &&hace_sol){
+              System.out.println("mejor me quedo en casa");
+      }
+          
+      }*/
+     /*7)boolean temp=true;
+     int temperatura=10;
+    if (temp) {
+
+if(temperatura<=25) {
+System.out.println("voy a la playa");
+
+    
+  }
+    }*/
+    /*8)boolean temp=true;
+     int temperatura=10;
+    if (temp) {
+
+if(temperatura>25) {
+System.out.println("voy a la playa");
+        } else {
+    System.out.println("espero el buen tiempo");
+
+
+
+}
+
+    
+  }
+    */
+    /*9) boolean temp=true,nevada=true;
+     int temperatura=10;
+     
+    if (temp) {
+
+if(temperatura>25) {
+System.out.println("A la playa");
+        } else{
+}
+
+if(temperatura>15){
+System.out.println("A la montaÃ±a");
+}
+ if (temperatura<5&&nevada);
+{
+    System.out.println("a esquiear");
+}
+    
+    
+    }
+    */
+     
+    boolean temp=true;
+     int temperatura=10;
+     if (temp) {
+     }
+     if(temperatura>15){
+     
+ } 
+   if(temperatura<5){ 
+System.out.println("A esquiar");       
+    } else {
+       System.out.println("A dormir zzz");
+   } 
+  if(temperatura >25){
+      System.out.println("A la playa");
+  } else {
+      System.out.println("A la montaña");
+  }
+}
+}
